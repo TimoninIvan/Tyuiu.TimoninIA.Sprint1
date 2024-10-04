965cc570ce561b3e842d09e4499c39e93cb8ee95
Ivan Timonin <Tyuiu.TimoninIA@outlook.com>
2024-10-04 20:26:47 +0600

    Task5

---
+++ Tyuiu.TimoninIA.Sprint1.Task5.V4/Program.cs
@@ -0,0 +1,46 @@
+﻿using Tyuiu.TimoninIA.Sprint1.Task5.V4.Lib;
+namespace Tyuiu.TimoninIA.Sprint1.Task5.V4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Тимонин Иван Александрович | ИИПБ-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* спринт #1                                                               *");
+            Console.WriteLine("* Тема : Преобразование типов и класс Convert                             *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #4                                                              *");
+            Console.WriteLine("* Выполнил: Тимонин Иван Александрович | ИИПБ-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая решает следующую задачу:                    *");
+            Console.WriteLine("* Идет k-я секунда суток. Определить, сколько полных                      *");
+            Console.WriteLine("* часов прошло к этому моменту                                            *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double sd = 86400;
+            double hd = 24;
+            int k;
+
+            Console.WriteLine("Введите значение прошедших секунд:");
+            k = Convert.ToInt32(Console.ReadLine());
+
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+
+            Console.WriteLine(ds.SecondsToHours(k));
+
+            Console.ReadLine();
+        }
+    }
+}
