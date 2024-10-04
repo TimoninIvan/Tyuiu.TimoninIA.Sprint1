6705d2a06fc683e0c01deffe7d34000a43da0276
Ivan Timonin <Tyuiu.TimoninIA@outlook.com>
2024-10-04 19:38:34 +0600

    Task3

---
+++ Tyuiu.TimoninIA.Sprint1.Task3.V7/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.TimoninIA.Sprint1.Task3.V7.Lib;
+
+namespace Tyuiu.TimoninIA.Sprint1.Task3.V7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Тимонин Иван Александрович | ИИПБ - 24-1";
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* Спринт #1                                                                                *");
+            Console.WriteLine("* Тема: Операторы составного присваивания                                                  *");
+            Console.WriteLine("* Задание #3                                                                               *");
+            Console.WriteLine("* Вариант #7                                                                               *");
+            Console.WriteLine("* Выполнил: Тимонин Иван Александрович | ИИПБ - 24-1                                       *");
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                                 *");
+            Console.WriteLine("* Написать консольную программу, которая запришивает у пользователя исходные данные,       *");
+            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.                              *");
+            Console.WriteLine("*                                                                                          *");
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                         *");
+            Console.WriteLine("********************************************************************************************");
+
+            double verst;
+            Console.WriteLine("Введите расстояние в верстах = ");
+            verst = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("*РЕЗУЛЬТАТ:                                                                                *");
+            Console.WriteLine("********************************************************************************************");
+
+            Console.WriteLine("100 верст - это " + ds.VerstsToKilometers(verst) + " км.");
+
+            Console.ReadLine();
+        }
+    }
+}
